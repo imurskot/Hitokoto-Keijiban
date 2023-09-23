0d03e26c6c22efc518fd72a0fdc61cb17ca01b57
Shima Kanako <simakana923@gmail.com>
2023-09-24 00:31:26 +0900

    ひとこと掲示板

---
+++ web-message-board/public/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="robots" content="noindex" />
+  <title>ひとこと掲示板</title>
+  <link rel="stylesheet" href="./assets/main.css" />
+</head>
+
+<body>
+  <div class="page-cover">
+    <p class="page-title">ひとこと掲示板</p>
+      <div class="form-cover">
+        <!-- 投稿内容入力フォーム -->
+      </div>
+    <hr class="page-divider" />
+      <div class="message-list-cover">
+        <!-- この中に投稿された内容のリストを表示 -->
+      </div>
+    <hr class="page-divider" />
+  </div>
+</body>
+
+</html>
