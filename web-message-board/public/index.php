f82976fd9afa16300117395342476ae0c4b62695
Shima Kanako <simakana923@gmail.com>
2023-09-27 23:44:27 +0900

    9/27

--- web-message-board/public/index.php
+++ web-message-board/public/index.php
@@ -1,3 +1,15 @@
+<?php
+session_start();
+require_once(__DIR__ . '/../public/src/db_connect.php');
+
+if (isset($_POST['action_type']) && $_POST['action_type']) {
+  if ($_POST['action_type'] === 'insert') {
+    require(__DIR__ . '/../public/src/insert_message.php');
+  }
+}
+
+require(__DIR__ . '/../public/src/session_values.php');
+?>
 <!DOCTYPE html>
 <html>
 
@@ -7,7 +19,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="robots" content="noindex" />
   <title>ひとこと掲示板</title>
-  <link rel="stylesheet" href="./assets/main.css" />
+  <link rel="stylesheet" href="./public/assets/main.css" />
 </head>
 
 <body>
@@ -18,19 +30,22 @@
     <div class="form-cover">
       <form action="/" method="post">
         <div class="form-input-title">投稿者ニックネーム</div>
-        <input type="text" name="author_name" maxlength="40" value="" class="input-author-name" />
-        <div class="form-input-error">
-        </div>
+        <input type="text" name="author_name" maxlength="40" value="<?php echo htmlspecialchars($messages['input_pre_author_name'], ENT_QUOTES); ?>" class="input-author-name" />
+        <?php if ($messages['input_error_author_name'] !== '') { ?>
+          <div class="form-input-error">
+            <?= $messages['input_error_author_name']; ?>
+          </div>
+        <?php } ?>
         <div class="form-input-title">投稿内容<small>(必須)</small></div>
-        <textarea name="message" class="input-message"></textarea>
-        <div class="form-input-error">
-        </div>
+        <textarea name="message" class="input-message"><?php echo htmlspecialchars($messages['input_pre_message'], ENT_QUOTES); ?></textarea>
+        <?php if ($messages['input_error_message'] !== '') { ?>
+          <div class="form-input-error">
+            <?= $messages['input_error_message']; ?>
+          </div>
+        <?php } ?>
         <input type="hidden" name="action_type" value="insert" />
         <button type="submit" class="input-submit-button">投稿する</button>
       </form>
     </div>
-  <hr class="page-divider" />
-  </div>
-</body>
 
-</html>
+    <hr class="page-divider" />
